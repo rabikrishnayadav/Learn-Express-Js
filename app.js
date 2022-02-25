d73f6c71b08d484d4cfd0d9879e572ff1f268e21
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-25 12:15:41 +0545

    first program

---
+++ app.js
@@ -0,0 +1,29 @@
+// npm init
+// touch app.js
+// npm install express
+
+const express = require('express');     // import express module
+
+const app = express();      // this function for express app
+
+// app.get(route, callback);
+
+/*
+The callback function has 2 paramenters, request(req) response(res).
+The request object(req) represents the HTTP request and
+has properties for the request query string, paramenters, body, HTTP headers, ec.
+
+Similarly, the response object represents the HTTP response that the express app sends when it receives an HTTP request
+*/
+
+app.get('/', (req, res) => {
+    res.send('hello from the express');
+});
+
+app.get('/about', (req, res) => {
+    res.send('hello from the express in about page');
+});
+
+app.listen(8000, () => {
+    console.log('listing the port at 8000');
+});
